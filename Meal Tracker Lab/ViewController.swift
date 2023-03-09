d303ae101d3b3ff6e7cbdb8e12ffa525dd70b0df
Paige Stephenson <paigestephenson29@gmail.com>
2023-03-09 11:06:02 -0700

    Initial Commit

---
+++ Meal Tracker Lab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meal Tracker Lab
+//
+//  Created by Paige Stephenson on 3/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
